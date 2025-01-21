09f1d40bed2dea1898cbefa5d35c5e9187ab87d8
Ozan <git@ozansap.com>
2025-01-21 23:56:51 +0300

    update encode/decode to match ingame

--- utils/code.ts
+++ utils/code.ts
@@ -50,250 +50,92 @@ const position = {
 	],
 };
 
-// finds out which characters and cards are in a deck image
-// unneeded if you already know the cards in the deck
-// returns [error, null] if there is an error
-// returns [null, CardsData] if there is no error
-export const extract = async (url: string): Promise<Unsure<CardsData>> => {
-	let cardsData: CardsData = {
-		version: cards.latest as Version,
-		characters: [],
-		deck: {},
-	};
+const scrambling = [
+	[0, 1, 4],
+	[5, 8, 9],
+	[12, 13, 16],
+	[17, 20, 21],
+	[24, 25, 28],
+	[29, 32, 33],
+	[36, 37, 40],
+	[41, 44, 45],
+	[48, 49, 52],
+	[53, 56, 57],
+	[60, 61, 64],
+	[65, 68, 69],
+	[72, 73, 76],
+	[77, 80, 81],
+	[84, 85, 88],
+	[89, 92, 93],
+	[96, 97, 2],
+	[3, 6, 7],
+	[10, 11, 14],
+	[15, 18, 19],
+	[22, 23, 26],
+	[27, 30, 31],
+	[34, 35, 38],
+	[39, 42, 43],
+	[46, 47, 50],
+	[51, 54, 55],
+	[58, 59, 62],
+	[63, 66, 67],
+	[70, 71, 74],
+	[75, 78, 79],
+	[82, 83, 86],
+	[87, 90, 91],
+	[94, 95, 98],
+]
 
-	const latest = cards[cards.latest as Version];
+export function encode(deck: number[], offset: number = 0): string {
+	let unscrambled = deck.map(d => d.toString(16).padStart(3, '0'));
+	let scrambled: string[] = [];
 
-	const response = await axios.get(url, { responseType: "arraybuffer" });
-	const buffer = Buffer.from(response.data, "utf-8");
-
-	for (const p of position.characters) {
-		const cropped = await sharp(buffer).extract(p).toBuffer();
-
-		for (let i = 0; i < latest.characters.length; i++) {
-			const char = latest.characters[i];
-
-			const options: ComparisonOptions = {
-				scaleToSameSize: true,
-				ignore: "antialiasing",
-				returnEarlyThreshold: 20,
-			};
-
-			const data = await compareImages(cropped, char.resource, options);
-
-			if (data.misMatchPercentage < 20) {
-				cardsData.characters.push(i);
-				break;
-			}
-		}
+	for (let i = 0; i < scrambling.length; i++) {
+		scrambling[i].forEach((s, j) => {
+			scrambled[s] = unscrambled[i][j];
+		})
 	}
 
-	if (cardsData.characters.length !== 3) {
-		return ["Could not find the character cards", null];
+	scrambled.push(...offset.toString(16).padStart(3, '0'))
+
+	let bytes = [];
+	for (let i = 0; i < scrambled.length; i += 2) {
+		let byte = scrambled[i] + scrambled[i + 1];
+		bytes.push(byte);
 	}
 
-	let cardCount = 0;
-	let index = 0;
-	for (const p of position.deck) {
-		const cropped = await sharp(buffer).extract(p).toBuffer();
+	let hex_code = hex_offset(bytes, offset).join("");
+	let buffer = Buffer.from(hex_code, 'hex');
+	return buffer.toString('base64');
+}
 
-		for (let i = index; i < latest.deck.length; i++) {
-			const card = latest.deck[i];
+export function decode(code: string): number[] {
+	let buffer = Buffer.from(code, 'base64');
+	let hex_code = buffer.toString('hex');
 
-			const options: ComparisonOptions = {
-				scaleToSameSize: true,
-				ignore: "antialiasing",
-				returnEarlyThreshold: 20,
-			};
-
-			const data = await compareImages(cropped, card.resource, options);
-
-			if (333000 < card.id) {
-				if (data.misMatchPercentage > 5) continue;
-				index = i;
-				cardsData.deck[i] ? cardsData.deck[i]++ : (cardsData.deck[i] = 1);
-				//console.log(`${++cardCount}: ${data.misMatchPercentage} - ${card.name}`);
-				break;
-			}
-
-			if (330000 < card.id && card.id < 331000) {
-				if (data.misMatchPercentage > 20) continue;
-				index = i;
-				cardsData.deck[i] ? cardsData.deck[i]++ : (cardsData.deck[i] = 1);
-				//console.log(`${++cardCount}: ${data.misMatchPercentage} - ${card.name}`);
-				break;
-			}
-
-			if (data.misMatchPercentage < 8) {
-				index = i;
-				cardsData.deck[i] ? cardsData.deck[i]++ : (cardsData.deck[i] = 1);
-				//console.log(`${++cardCount}: ${data.misMatchPercentage} - ${card.name}`);
-				break;
-			}
-		}
+	let bytes = [];
+	for (let i = 0; i < hex_code.length; i += 2) {
+		let byte = hex_code[i] + hex_code[i + 1];
+		bytes.push(byte);
 	}
 
-	if (Object.values(cardsData.deck).reduce((a, b) => a + b, 0) !== 30) {
-		return ["Could not find the action cards", null];
+	let offset = parseInt(bytes[bytes.length - 1], 16);
+	let scrambled = hex_offset(bytes, -offset).join("");
+	let unscrambled = [];
+
+	for (let i = 0; i < scrambling.length; i++) {
+		unscrambled.push(scrambling[i].map(s => scrambled[s]).join(""));
 	}
 
-	return [null, cardsData];
-};
+	return unscrambled.map(s => parseInt(s, 16));
+}
 
-// returns [error, null] if there is an error
-// returns [null, DeckCode (string)] if there is no error
-export const encode = (data: CardsData): Unsure<string> => {
-	try {
-		const l = cards.latest as Version;
-		const latest = cards[l];
+function hex_offset(bytes: string[], offset: number) {
+	let new_bytes = bytes.map(hex => {
+		let byte = parseInt(hex, 16);
+		byte = (byte + offset + 256) % 256;
+		return byte.toString(16).padStart(2, '0');
+	})
 
-		// base 3 number that holds information on non character cards in the deck
-		let field = Array(latest.deck.length).fill(0);
-
-		// fill the array in reverse so that it works properly
-		for (const i in data.deck) {
-			field[reverse(field, parseInt(i))] = data.deck[i];
-		}
-
-		const deckCode = encodeHeader(l) + encodeCharacters(data.characters) + encodeDeck(field.join(""));
-		return [null, deckCode];
-	} catch (err) {
-		return ["Could not convert into deck code", null];
-	}
-};
-
-// returns [error, null] if there is an error
-// returns [null, CardsData] if there is no error
-export const decode = (code: string): Unsure<CardsData> => {
-	try {
-		// first 2 chars of the code is the version
-		const version = decodeHeader(code.slice(0, 2));
-
-		// finds the characters from first 3 variable length numbers
-		// returns indexes of characters and the rest of the code converted to base 3
-		const { characters, deckCode } = decodeCards(code.slice(2));
-
-		let cardsData: CardsData = {
-			version,
-			characters,
-			deck: {},
-		};
-
-		// loop in reverse
-		for (let i = deckCode.length - 1; i >= 0; i--) {
-			const value = deckCode[i];
-
-			// last digit of deckCode is actually the first card in the array
-			const index = reverse(deckCode, i);
-
-			if (value !== "0") {
-				cardsData.deck[index] = parseInt(value);
-			}
-		}
-
-		return [null, cardsData];
-	} catch (err) {
-		return ["Could not convert your input to a deck", null];
-	}
-};
-
-export const display = (data: CardsData): string => {
-	const version = cards[data.version];
-
-	let description = data.characters.map((i) => version.characters[i].name).join(" - ") + "\n";
-
-	for (const index in data.deck) {
-		const value = data.deck[index];
-		description += `${value} - ${version.deck[parseInt(index)].name}\n`;
-	}
-
-	description += "\n⚠️ This deck code has no function right now";
-
-	return description;
-};
-
-const reverse = (arr: any[] | string, n: number): number => {
-	return arr.length - 1 - n;
-};
-
-const encodeHeader = (x: string): string => {
-	return convertBase(x, 10, chars).padStart(2, "0");
-};
-
-// xs: array of character indexes
-const encodeCharacters = (xs: number[]): string => {
-	let res = "";
-
-	// for each character: divide index by 62
-	// quotient rounded down times "z"
-	// remainder is converted to base 62
-	for (const x of xs) {
-		res += chars[chars.length - 1].repeat(Math.floor(x / chars.length));
-		res += chars[x % chars.length];
-	}
-
-	return res;
-};
-
-// ~150 digit number is too big for JS to handle, use string
-const encodeDeck = (x: string): string => {
-	return convertBase(x, "012", chars);
-};
-
-const decodeHeader = (x: string): Version => {
-	return convertBase(x, chars, 10) as Version;
-};
-
-const decodeCards = (x: string) => {
-	let characters = [];
-	let characterLength = 0;
-
-	// we don't know the length of the part that contains information on characters
-	// so this runs until 3 characters are found
-	// a character is found when a char other than "z" is read
-	for (let i = 0; i < x.length; i++) {
-		let characterCode = x[i];
-
-		// if char is "z" next char belongs to this character too
-		while (characterCode[characterCode.length - 1] === chars[chars.length - 1]) {
-			characterCode += x[++i];
-		}
-
-		characters.push(decodeCharacter(characterCode));
-		characterLength = i;
-
-		if (characters.length === 3) break;
-	}
-
-	return {
-		characters,
-		deckCode: convertBase(x.slice(characterLength + 1), chars, "012"),
-	};
-};
-
-// x: variable length number in base 62
-// "3" ⟶ 3; "A" ⟶ 10; "z0" ⟶ 62; "zzz6" ⟶ 193
-const decodeCharacter = (x: string): number => {
-	let n = 0;
-
-	for (let i = 0; i < x.length; i++) {
-		n += chars.indexOf(x[i]);
-	}
-
-	return n;
-};
-
-// characters: an array of length 3, elements are the indexes of characters
-// deck: (index of the non character card): (how many copies of that card are in the deck)
-type CardsData = {
-	version: Version;
-	characters: number[];
-	deck: {
-		[index: string]: number;
-	};
-};
-
-// version number as string
-// must be a key of cards from cards.json
-type Version = Exclude<keyof typeof cards, "latest">;
-
-type Unsure<T> = [error: null, result: T] | [error: string, result: null];
+	return new_bytes;
+}
